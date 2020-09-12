6196b1af61a1291e259e9e884f2ab97a4ae1b372
Jasper Fitzgerald-Cherry <jasper.cherry@gmail.com>
2020-09-13 01:37:43 +1000

    Update Population.cpp

--- Population.cpp
+++ Population.cpp
@@ -1,25 +1,52 @@
 #include <cstdio>
 #include <vector>
 #include <iostream>
-
+#include <curses.h>
+#include <unistd.h>
 
 int main() {
     
-    std::vector<int> sequence;
+    int tmp_population = 0;
+    double tmp_growth_rate = 0;
+    char key_input;
     
-    // initial values
-    int initialPopulation = 50000000;
-    int quantityOfYears = 25;
-    double growthRate = 1.03;
+    int counter = 0;
     
-    std::cout << "Initial population >> " << initialPopulation << std::endl;
-    std::cout << "Quantity of years >> " << quantityOfYears << std::endl;
-    std::cout << "Growth rate >> " << growthRate << std::endl;
+    while (true) {
+        std::cout << "what is the initial population?" << std::endl;
+        std::cin >> tmp_population;
     
-    // this captures the population growth after a certain period of time
-    for (int ii=0; ii < quantityOfYears; ++ii) {
-        initialPopulation = initialPopulation * growthRate;
-        sequence.push_back(initialPopulation);
-        std::cout << "Pop(" << ii + 1 << ") = " << sequence[ii] << std::endl;
+        std::cout << "what is the growth rate?" << std::endl;
+        std::cin >> tmp_growth_rate;
+    
+        if (tmp_population == 0 || tmp_growth_rate == 0) {
+            std::cout << "ERROR: invalid numbers!!!" << std::endl;
+        }
+        
+        // check the quantity of years for growth
+        std::cout << "What is the quantity of years you want to check for growth?" << std::endl;
+    
+        std::cout << "Now hit return multiple times to see results." << std::endl;
+        
+        for (int p_inc=0; p_inc < 100000; ++p_inc) {
+            
+            key_input = std::cin.get();
+            
+            if (key_input) {
+                counter += 1;
+            }
+            
+            while (counter >= 1) {
+                if (key_input) {
+                    break;
+                } else {
+                    counter--;
+                }
+            }
+            tmp_population = tmp_population * tmp_growth_rate;
+            printf("Year = %d, Population = %0.0d ", counter, tmp_population);
+            
+        }
     }
+    
 }
